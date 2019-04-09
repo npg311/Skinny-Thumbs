7cecadbe9f145c4c6eef33747ed54bc0d8572d2f
npg311 <47582847+npg311@users.noreply.github.com>
2019-04-09 18:55:18 -0500

    new styles

---
+++ assets/javaScript/project.js
@@ -0,0 +1,4 @@
+//var queryURL = "https://api.pinterest.com/v1/me/pins/?" + access_token &fields=id,creator,note, &limit=1 
+//access_token=AuTwntNjP9M-T30Vu_537mFHKGhNFZLzZplff6RFv40N_AC7jgZGQDAAAMFtRcAKlIeApPkAAAAA
+//&fields=id,creator,note
+//&limit=1
